ac51ae9438980c22d2114ccff9dd926f19980313
juaniloco <juanibrunero@gmail.com>
2019-11-01 15:44:06 -0300

    Solution error not conection

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var port = process.argv[2];
-
 var express = require('express');
 var mongoose = require('mongoose');
 var body_parser = require('body-parser');
 var user = require('./routes/user');
 var user_directory = require('./routes/user_directory');
+
 require('dotenv').config()
 
-mongoose.connect('mongodb://'+process.env.IPDB+':27017/zeus', {useNewUrlParser:true, useUnifiedTopology: true });
+mongoose.connect('mongodb://'+process.env.IPDB+':27017/zeuz', {useNewUrlParser:true, useUnifiedTopology: true }).
+catch(error => console.log(error));
 
 var app = express();
 app.use(body_parser.json());
